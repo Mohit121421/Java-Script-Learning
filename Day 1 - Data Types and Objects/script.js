f721cb80ec9db021d702b84bc245100e8fa7e4a4
Mohit Khandale <138105871+Mohit121421@users.noreply.github.com>
2025-01-06 20:53:31 +0530

    Add files via upload

---
+++ Day 1 - Data Types and Objects/script.js
@@ -0,0 +1,32 @@
+console.log('Hello, world!');
+var a = 5;
+var b = 10;
+var c = "Mohit";
+
+console.log(a + b + 8);
+
+console.log(c);
+
+console.log(typeof a, typeof c, typeof b);//Shows the type of a variable
+
+const a1=6;//Cant change the value of a constant
+
+let x = "Mohit bhai";
+let y =22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+//Object creation
+let obj={
+    "name": "Mohit",
+    "age": 22,
+    "city": "Nashik"
+}
+console.log(obj);
+obj.salary = "100 lakhs";//adding keys on object 'obj'
+console.log(obj);
