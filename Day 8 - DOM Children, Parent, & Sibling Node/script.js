34135a94e0f6a00f35dad4d2d9f20c0b11a7feaf
Mohit Khandale <138105871+Mohit121421@users.noreply.github.com>
2025-01-14 11:48:12 +0530

    Add files via upload

---
+++ Day 8 - DOM Children, Parent, & Sibling Node/script.js
@@ -0,0 +1,16 @@
+console.log('Hello, Today we are going to learn about JavaScript DOM Children, Parent, & Siblings.'); 
+let cont = document.body
+console.log(cont.firstChild)
+console.log(cont.lastChild)
+console.log(cont.childNodes)
+
+console.log(document.body.firstElementChild)
+console.log(document.body.nextElementSibling)
+console.log(document.body.lastElementChild)
+
+console.log(document.getElementById)
+console.log(document.querySelectorAll)
+console.log(document.querySelector)
+console.log(document.getElementsByTagName)
+console.log(document.getElementsByClassName)
+console.log(document.getElementsByName)
