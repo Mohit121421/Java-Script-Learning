8e02bbde479ec4f98675db6de2bee69fe3140000
Mohit Khandale <138105871+Mohit121421@users.noreply.github.com>
2025-01-16 21:31:26 +0530

    Add files via upload

---
+++ Day 11 - Events, Event Bubbling, setIntervals & setTimeout/script.js
@@ -0,0 +1,30 @@
+let button = document.getElementById("btn")
+//Single Click
+/*
+//Single Click
+button.addEventListener("click", ()=>{
+    alert("You Want To Change Content");
+    document.querySelector(".box").innerHTML = "<b>Ready To Go....</b>"
+})*/
+
+/*
+//Double Click
+button.addEventListener("dblclick", ()=>{
+    alert("You Want To Change Content");
+    document.querySelector(".box").innerHTML = "<b>Ready To Go....</b>"
+})
+*/
+
+/*
+//Mouse Right click
+button.addEventListener("contextmenu", ()=>{
+    alert("Don't Right click");
+})
+ */
+
+/*
+//KeyDown
+button.addEventListener("keydown", (e)=>{
+    console.log(e);
+})
+*/
